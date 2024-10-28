ed92daf95139e15bda0ecc9db6840ec1309083da
TheMallus <alostaunauwave@gmail.com>
2024-10-28 09:57:44 -0500

    collision detection added

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-//! A simple 3D scene with light shining over a cube sitting on a plane.
+use std::ops::Range;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::HashMap};
+use rand::Rng;
+use bevy::window::PrimaryWindow;
+use bevy_inspector_egui::{quick::WorldInspectorPlugin, bevy_egui::EguiContext};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(WorldInspectorPlugin::default())
         .add_systems(Startup, setup)
         .add_systems(PostStartup, spawn_block)
         .add_systems(Update, (update_velocity, update_position))
         .add_systems(Update, block_movement_controls)
-        .add_systems(Update, print_position)
+        .add_systems(Update, (print_position, print_health))
+        .add_systems(Update, collision_detection)
+        .add_systems(Update, block_weapons)
+        .add_systems(Update, despawn_far_away_entities)
+        .add_systems(Update, spawn_dummy)
+        .add_systems(Update, handle_collisions)
+        .add_systems(Update, health_ui)
         .run();
 }
 
@@ -24,7 +34,7 @@ fn setup(
 ) {
     // circular base
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Circle::new(4.0)),
+        mesh: meshes.add(Circle::new(100.0)),
         material: materials.add(Color::WHITE),
         transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
         ..default()
@@ -41,7 +51,7 @@ fn setup(
     });
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-10.5, 7.5, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
@@ -89,6 +99,34 @@ impl Acceleration {
     }
 }
 
+#[derive(Component, Debug)]
+pub struct Health {
+    pub value: f32,
+}
+
+impl Health {
+    pub fn new(value: f32) -> Self {
+        Self { value }
+    }
+}
+
+// const DAMAGE: f32 = 0.5;
+// fn update_health (
+//     mut query: Query<(Entity, &mut Health)>,
+//     mut commands: Commands
+// )
+// {
+//     for (entity, mut health) in query.iter_mut() {
+//         health.value += -DAMAGE;
+//         if health.value <= 0.0 {
+//             commands.entity(entity).despawn_recursive();
+//             continue
+//         }
+//     }
+// }
+// damahe component is based on collision, possibly use rapier for this or else use raycast or the demo
+// collision system that isn't run with a bevy engine and hard coded but actually works
+
 // Components are great for organizing various items for an entity(like the block)
 // but we can organize them further by grouping them into bundles
 // bundles are great for categorizing your variables into different parts
@@ -103,6 +141,8 @@ pub struct MovingObjBundle {
     pub velocity: Velocity,
     pub acceleration: Acceleration,
     pub model: PbrBundle, // this is a built in bevy bundle
+    pub health: Health,
+    pub collider: Collider,
 }
 
 // now that we have our block bundle let's spawn in the block!
@@ -122,7 +162,10 @@ pub fn spawn_block(
                 material: materials.add(Color::BLACK), // makes the cube black
                 transform: Transform::from_xyz(0.0, 0.5, 0.0),
                 ..default() // Transform describes the position of the block
-            },              // this will spawn the block at the xyz coords (0.0, 0.5, 0.0)
+            },
+            health: Health::new(1000.0), 
+            collider: Collider::new(1.0),             
+                            // this will spawn the block at the xyz coords (0.0, 0.5, 0.0)
         },                  // the default sets the rest of the PbrBundle components to
                             // a built in default variable
         Block,   // <---- see below for why this is here
@@ -252,3 +295,167 @@ fn print_position(query: Query<(Entity, &Transform)>) {
     } // function basically prints location of ALL existing entities per frame
 }
 
+fn print_health(query: Query<(Entity, &Health)>) {
+    for  (entity, health) in query.iter() {
+        info!("Entity {:?} has {:?} health.", entity, health.value);
+    }
+}
+
+
+
+
+#[derive(Component, Debug)]
+pub struct Collider {
+    pub radius: f32,
+    pub colliding_entities: Vec<Entity>,
+}
+
+impl Collider {
+    pub fn new(radius: f32) -> Self {
+        Self {
+            radius,
+            colliding_entities: vec![],
+        }
+    }
+}
+
+fn collision_detection(mut query: Query<(Entity, &GlobalTransform, &mut Collider)>) {
+    let mut colliding_entities: HashMap<Entity, Vec<Entity>> = HashMap::new();
+
+    for (entity_a, transform_a, collider_a) in query.iter() {
+        for (entity_b, transform_b, collider_b) in query.iter() {
+            if entity_a != entity_b {
+                let distance = transform_a.translation().distance(transform_b.translation());
+                if distance < collider_a.radius + collider_b.radius {
+                    colliding_entities
+                        .entry(entity_a)
+                        .or_insert_with(Vec::new)
+                        .push(entity_b);
+                }
+            }
+        }
+    }
+
+    for (entity, _, mut collider) in query.iter_mut() {
+        collider.colliding_entities.clear();
+        if let Some(collisions) = colliding_entities.get(&entity) {
+            collider.colliding_entities.extend(collisions.iter().copied());
+        }
+    }
+
+}
+
+#[derive(Component, Debug)]
+pub struct Bullet;
+
+fn block_weapons(
+    mut commands: Commands,
+    query: Query<&mut Transform, With<Block>>, 
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) 
+{
+    let transform = query.single();
+    if keyboard_input.pressed(KeyCode::KeyF) {
+        commands.spawn((MovingObjBundle {
+            velocity: Velocity::new(-transform.forward() * 25.0),
+            acceleration: Acceleration::new(Vec3::ZERO),
+            collider: Collider::new(1.0),
+            health: Health::new(100.0),
+            model: PbrBundle {
+                mesh: meshes.add(Cuboid::new(0.1,0.1,0.1)),
+                material: materials.add(Color::BLACK),
+                transform: Transform::from_translation(
+                    transform.translation + -transform.forward() * 1.1,
+                ),
+                ..default()
+            },
+        },
+        Bullet,
+        ));
+    }
+
+}
+
+fn despawn_far_away_entities(mut commands: Commands, query: Query<(Entity, &GlobalTransform)>) {
+    for (entity, transform) in query.iter() {
+        let distance = transform.translation().distance(Vec3::ZERO);
+
+        if distance > 25.0 {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn handle_collisions(mut commands: Commands, query: Query<(Entity, &Collider), With<Dummy>>,) {
+    for (entity, collider) in query.iter() {
+        for &collided_entity in collider.colliding_entities.iter() {
+            if query.get(collided_entity).is_ok() {
+                continue;
+            }
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn spawn_dummy(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+
+    let mut rng = rand::thread_rng();
+    let translation = Vec3::new(rng.gen_range(SPAWN_RANGE_X), 0.5, rng.gen_range(SPAWN_RANGE_Z),);
+
+    let mut random_unit_vector = || Vec3::new(rng.gen_range(-1.0..1.0), 0., rng.gen_range(-1.0..1.0)).normalize_or_zero();
+    let velocity = random_unit_vector() * VELOCITY_SCALAR;
+    let acceleration = random_unit_vector() * ACCELERATION_SCALAR;
+    
+
+    if keyboard_input.pressed(KeyCode::KeyJ) {
+        commands.spawn(
+            (MovingObjBundle {
+                velocity: Velocity::new(velocity),         // recall our functions we made this will just
+                acceleration: Acceleration::new(acceleration), // make a '0.0' 3d vector like this (0.0, 0.0, 0.0)
+                model: PbrBundle {
+                    mesh: meshes.add(Cuboid::new(1.0,1.0,1.0)), // makes the cube
+                    material: materials.add(Color::srgb(255.0,0.0,0.0)), // makes the cube black
+                    transform: Transform::from_translation(translation),
+                    ..default() // Transform describes the position of the block
+                },
+                health: Health::new(100.0), 
+                collider: Collider::new(1.0),             
+                                // this will spawn the block at the xyz coords (0.0, 0.5, 0.0)
+            },                  // the default sets the rest of the PbrBundle components to
+                                // a built in default variable
+            Dummy,   // <---- see below for why this is here
+        ));
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Dummy;
+
+
+const VELOCITY_SCALAR: f32 = 5.0;
+const ACCELERATION_SCALAR: f32 = 1.0;
+const SPAWN_RANGE_X: Range<f32> = -5.0..5.0;
+const SPAWN_RANGE_Z: Range<f32> = 0.0..5.0;
+
+fn health_ui(
+    mut primary_window: Query<&mut EguiContext, With<PrimaryWindow>>,
+    entity_health: Query<(Entity, &Health), Without <Bullet>>,
+) {
+    for mut context in primary_window.iter_mut() {
+        egui::Window::new("Health").show(context.get_mut(), |ui| {
+            for (e, health) in entity_health.iter() {
+                ui.heading(format!(
+                    "{:#?}'s Health: {:#}",
+                    e, health.value
+                ));
+            }
+        });
+    }
+}
